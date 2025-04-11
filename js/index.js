39939692998c780ca02daf487e0056da5779a1e4
ennio <ennio.nick@gmail.com>
2025-04-11 11:51:33 +0200

    toggle switch logic added

--- js/index.js
+++ js/index.js
@@ -1,5 +1,11 @@
 
 document.addEventListener('DOMContentLoaded', () => {
 
-
+    document.querySelector('.switch input[type="checkbox"]').addEventListener('change', function () {
+        toggleLedSwitch()
+    });
 })
+
+function toggleLedSwitch() {
+    ledButtonToggled = this.checked;
+}
